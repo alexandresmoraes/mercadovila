2ae59e066150ca4046f6db2bd2cbef05ac3624eb
Alexandre Moraes <alexandresmoraes@me.com>
2023-05-04 23:46:34 -0300

    mobile: initial commit

---
+++ src/mobile/lib/app/modules/home/home_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'home_store.dart';
+
+class HomePage extends StatefulWidget {
+  final String title;
+  const HomePage({Key? key, this.title = 'Home'}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  late final HomeStore store;
+
+  @override
+  void initState() {
+    super.initState();
+    store = Modular.get<HomeStore>();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter'),
+      ),
+      body: Observer(
+        builder: (context) => Text('${store.counter}'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          store.increment();
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
