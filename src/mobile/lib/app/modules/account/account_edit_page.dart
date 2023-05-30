d07fb56dd1159b9f2e1cb65506549b9269f4124a
Alexandre Moraes <alexandresmoraes@me.com>
2023-05-30 12:30:41 -0300

    up: removido referencias não usadas

--- src/mobile/lib/app/modules/account/account_edit_page.dart
+++ src/mobile/lib/app/modules/account/account_edit_page.dart
@@ -1,6 +1,4 @@
-import 'dart:convert';
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
