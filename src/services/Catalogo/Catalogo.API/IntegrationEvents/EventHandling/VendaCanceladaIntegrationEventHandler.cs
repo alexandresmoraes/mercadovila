e834d0e42f072b9f1e93692baedac32ea08fd4a5
Alexandre de Moraes <alexandresmoraes@me.com>
2024-02-07 10:24:18 -0300

    fix: correções volta no estoque

--- src/services/Catalogo/Catalogo.API/IntegrationEvents/EventHandling/VendaCanceladaIntegrationEventHandler.cs
+++ src/services/Catalogo/Catalogo.API/IntegrationEvents/EventHandling/VendaCanceladaIntegrationEventHandler.cs
@@ -9,14 +9,17 @@ namespace Catalogo.API.IntegrationEvents.EventHandling
     private readonly ILogger<VendaCanceladaIntegrationEvent> _logger;
     private readonly IProdutoRepository _produtoRepository;
 
+    public VendaCanceladaIntegrationEventHandler(ILogger<VendaCanceladaIntegrationEvent> logger, IProdutoRepository produtoRepository)
+    {
+      _logger = logger;
+      _produtoRepository = produtoRepository;
+    }
 
     public async Task Handle(VendaCanceladaIntegrationEvent @event)
     {
       _logger.LogInformation("----- Handling integration event: {IntegrationEventId} - ({@IntegrationEvent})", @event.Id, @event);
 
       await _produtoRepository.EntradaEstoqueAsync(@event.VendaItens.ToDictionary(_ => _.ProdutoId, _ => _.Quantidade));
-
-      throw new NotImplementedException();
     }
   }
 }
