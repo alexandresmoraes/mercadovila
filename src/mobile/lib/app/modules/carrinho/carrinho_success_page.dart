02db23b01cb1fc200e5a5eb0262f9035d06abac8
Alexandre de Moraes <alexandresmoraes@me.com>
2024-03-21 08:54:26 -0300

    up: remove PROBLEMS vscode

--- src/mobile/lib/app/modules/carrinho/carrinho_success_page.dart
+++ src/mobile/lib/app/modules/carrinho/carrinho_success_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:vilasesmo/app/modules/tab/tab_module.dart';
 
 class CarrinhoSuccessPage extends StatefulWidget {
