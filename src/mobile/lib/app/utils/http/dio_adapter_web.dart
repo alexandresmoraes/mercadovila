15bff2526cf6295ae4ec58f1a4ff088c6d7e881e
Alexandre de Moraes <alexandresmoraes@me.com>
2024-04-08 10:08:12 -0300

    up: removendo validação de certificado web em debug

--- src/mobile/lib/app/utils/http/dio_adapter_web.dart
+++ src/mobile/lib/app/utils/http/dio_adapter_web.dart
@@ -1,4 +1,9 @@
 import 'package:dio/browser.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 
-HttpClientAdapter getAdapter() => BrowserHttpClientAdapter();
+HttpClientAdapter getAdapter() {
+  var browserHttpClientAdapter = BrowserHttpClientAdapter();
+  if (kDebugMode) browserHttpClientAdapter.withCredentials = false;
+  return browserHttpClientAdapter;
+}
