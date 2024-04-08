321942002bd1f8c4c930bea709a12393d46f618e
Alexandre de Moraes <alexandresmoraes@me.com>
2024-04-08 10:43:44 -0300

    up: removendo validação certificado mobile adapter

--- src/mobile/lib/app/utils/http/dio_adapter_mobile.dart
+++ src/mobile/lib/app/utils/http/dio_adapter_mobile.dart
@@ -1,4 +1,18 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 import 'package:dio/io.dart';
+import 'package:flutter/foundation.dart';
 
-HttpClientAdapter getAdapter() => IOHttpClientAdapter();
+HttpClientAdapter getAdapter() {
+  var httpClientAdapter = IOHttpClientAdapter();
+  if (kDebugMode) {
+    httpClientAdapter.createHttpClient = () {
+      final HttpClient client = HttpClient(context: SecurityContext(withTrustedRoots: false));
+      client.badCertificateCallback = ((X509Certificate cert, String host, int port) => true);
+      return client;
+    };
+  }
+
+  return httpClientAdapter;
+}
