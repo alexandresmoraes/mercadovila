1734b7de068a0eec4ced27ee6fb80d8779ba9216
Alexandre de Moraes <alexandresmoraes@me.com>
2024-05-02 10:47:43 -0300

    up: adicionado mês de referência na tela Meus Pagamentos (#95)

--- src/mobile/lib/app/modules/meus_pagamentos/meus_pagamentos_module.dart
+++ src/mobile/lib/app/modules/meus_pagamentos/meus_pagamentos_module.dart
@@ -1,12 +1,15 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mercadovila/app/modules/meus_pagamentos/meus_pagamentos_page.dart';
+import 'package:mercadovila/app/modules/pagamentos/pagamentos_pagar_controller.dart';
 import 'package:mercadovila/app/utils/guard/auth_guard.dart';
 
 class MeusPagamentosModule extends Module {
   static const routeName = '/meus-pagamentos/';
 
   @override
-  final List<Bind> binds = [];
+  final List<Bind> binds = [
+    Bind.factory((i) => PagamentosPagarController()),
+  ];
 
   @override
   final List<ModularRoute> routes = [
