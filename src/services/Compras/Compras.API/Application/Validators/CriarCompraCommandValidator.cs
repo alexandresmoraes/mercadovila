b74768b5849ff5e65ef2da5defcbb889a2156958
Alexandre de Moraes <alexandresmoraes@me.com>
2024-05-03 16:18:39 -0300

    ref: refatorações e update de package

--- src/services/Compras/Compras.API/Application/Validators/CriarCompraCommandValidator.cs
+++ src/services/Compras/Compras.API/Application/Validators/CriarCompraCommandValidator.cs
@@ -3,12 +3,12 @@ using FluentValidation;
 
 namespace Compras.API.Application.Validators
 {
-  public class CriarCompraCommandValidator : AbstractValidator<CriarCompraCommand>
+  public sealed class CriarCompraCommandValidator : AbstractValidator<CriarCompraCommand>
   {
     public CriarCompraCommandValidator()
     {
       RuleFor(c => c.UsuarioNome)
-        .NotEmpty().WithMessage("O nome do comprador é obrigatório");
+        .NotEmpty().WithMessage("O nome do comprador é obrigatório.");
 
       RuleFor(x => x.CompraItens)
         .NotNull().WithMessage("A lista de itens não pode ser nula.")
@@ -20,21 +20,21 @@ namespace Compras.API.Application.Validators
     }
   }
 
-  public class CriarComparItemCommandValidator : AbstractValidator<CriarComparItemCommand>
+  public sealed class CriarComparItemCommandValidator : AbstractValidator<CriarComparItemCommand>
   {
     public CriarComparItemCommandValidator()
     {
-      RuleFor(c => c.ProdutoId).NotEmpty().WithMessage("O id do produto é obrigatório");
+      RuleFor(c => c.ProdutoId).NotEmpty().WithMessage("O id do produto é obrigatório.");
 
-      RuleFor(c => c.Nome).NotEmpty().WithMessage("O nome do produto é obrigatório");
+      RuleFor(c => c.Nome).NotEmpty().WithMessage("O nome do produto é obrigatório.");
 
-      RuleFor(c => c.ImageUrl).NotEmpty().WithMessage("A imagem do produto é obrigatório");
+      RuleFor(c => c.ImageUrl).NotEmpty().WithMessage("A imagem do produto é obrigatório.");
 
-      RuleFor(c => c.Descricao).NotEmpty().WithMessage("O descrição do comprador é obrigatório");
+      RuleFor(c => c.Descricao).NotEmpty().WithMessage("O descrição do comprador é obrigatório.");
 
       RuleFor(c => c.PrecoPago)
-        .NotEmpty().WithMessage("O preço pago do produto é obrigatório")
-        .GreaterThan(0).WithMessage("O preço pago do produto deve ser menor ou igual a zero");
+        .NotEmpty().WithMessage("O preço pago do produto é obrigatório.")
+        .GreaterThan(0).WithMessage("O preço pago do produto deve ser menor ou igual a zero.");
 
       RuleFor(c => c.PrecoSugerido)
         .NotEmpty().When(c => !c.IsPrecoMedioSugerido)
@@ -43,10 +43,10 @@ namespace Compras.API.Application.Validators
         .WithMessage("O preço sugerido é obrigatório quando IsPrecoSugerido = true.");
 
       RuleFor(c => c.Quantidade)
-        .NotNull().WithMessage("A quantidade do produto é obrigatório")
-        .GreaterThan(0).WithMessage("A quantidade do produto é obrigatório");
+        .NotNull().WithMessage("A quantidade do produto é obrigatório.")
+        .GreaterThan(0).WithMessage("A quantidade do produto é obrigatório.");
 
-      RuleFor(c => c.UnidadeMedida).NotEmpty().WithMessage("A unidade de medida do produto é obrigatório");
+      RuleFor(c => c.UnidadeMedida).NotEmpty().WithMessage("A unidade de medida do produto é obrigatório.");
     }
   }
 }
--- src/services/Vendas/Vendas.API/Application/Validators/RealizarPagamentoCommandValidator.cs
+++ src/services/Vendas/Vendas.API/Application/Validators/RealizarPagamentoCommandValidator.cs
@@ -4,27 +4,27 @@ using Vendas.Domain.Aggregates;
 
 namespace Vendas.API.Application.Validators
 {
-  public class RealizarPagamentoCommandValidator : AbstractValidator<RealizarPagamentoCommand>
+  public sealed class RealizarPagamentoCommandValidator : AbstractValidator<RealizarPagamentoCommand>
   {
     public RealizarPagamentoCommandValidator()
     {
-      RuleFor(c => c.UserId).NotEmpty().WithMessage("O id do comprador é obrigatório");
+      RuleFor(c => c.UserId).NotEmpty().WithMessage("O id do comprador é obrigatório.");
 
       RuleFor(c => c.TipoPagamento)
         .Cascade(CascadeMode.Stop)
         .NotNull().WithMessage("O tipo pagamento não pode ser nulo.");
 
       RuleFor(c => c.TipoPagamento)
-        .Must(c => c == null || Enum.IsDefined(typeof(EnumTipoPagamento), c.Value))
+        .Must(c => c is null || Enum.IsDefined(typeof(EnumTipoPagamento), c.Value))
         .WithMessage("O valor fornecido não é válido para a enumeração tipo pagamento.");
 
       RuleFor(c => c.MesReferencia)
        .Cascade(CascadeMode.Stop)
        .NotNull().WithMessage("O mês de refêrencia não pode ser nulo.");
 
-      RuleFor(c => c.TipoPagamento)
+      RuleFor(c => c.MesReferencia)
         .Must(c => c == null || Enum.IsDefined(typeof(EnumMesReferencia), c.Value))
-        .WithMessage("O valor fornecido não é válido para a enumeração tipo pagamento.");
+        .WithMessage("O valor fornecido não é válido para a enumeração mês de referência.");
 
       RuleFor(c => c.VendasId)
         .NotNull().WithMessage("A lista de id de vendas não pode ser nula.")
