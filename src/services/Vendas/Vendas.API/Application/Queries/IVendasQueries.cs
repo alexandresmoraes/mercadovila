4657be154e0cc1d230d3e97dae0303396edc6e5a
Alexandre de Moraes <alexandresmoraes@me.com>
2024-07-26 09:55:12 -0300

    up: adicionado opção de pagamento em dinheiro.

--- src/services/Vendas/Vendas.API/Application/Queries/IVendasQueries.cs
+++ src/services/Vendas/Vendas.API/Application/Queries/IVendasQueries.cs
@@ -9,5 +9,6 @@ namespace Vendas.API.Application.Queries
     Task<PagedResult<VendaDto>> GetMinhasComprasAsync(VendaQuery vendaQuery, string userId, CancellationToken cancellationToken = default);
 
     Task<PagedResult<VendaDto>> GetVendasAsync(VendaQuery vendaQuery, CancellationToken cancellationToken = default);
+    Task<bool> PagoEmDinheiro(long vendaId, CancellationToken cancellationToken = default);
   }
 }
